a393395cdcc4fa51d7b36da88274adad05eaa7da
MJayJ <m.a.jaworski96@gmail.com>
2017-11-28 12:27:32 +0100

    Added Components project.

---
+++ MyFantastiLibrary/Components/Usage/ReversingManipulator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReversingManipulator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReversingManipulator")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89368d99-9e98-4e0b-a6bf-feb8a4d77fe4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ IDE: Visual Studio Community 2017
 Platforms: Windows
 
 # PROJECTS #
+Components - creating and using of indepentent Components, Version: Not released yet
