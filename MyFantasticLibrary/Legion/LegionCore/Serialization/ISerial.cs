dd558091b2af25eec8d560e0f58eb231515970de
Marcin Jaworski <m.a.jaworski96@gmail.com>
2018-03-19 23:26:53 +0100

    JSON and XML Serialization/Deserialization.

---
+++ MyFantasticLibrary/Legion/LegionCore/Serialization/ISerial.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace LegionCore.Serialization
+{
+    public interface ISerial
+    {
+        void Serialize(object value, Type type, StreamWriter streamWriter);
+        object Deserialize(Type type, StreamReader streamReader);
+    }
+}
---
+++ MyFantasticLibrary/Legion/LegionCore/Serialization/JsonSerial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace LegionCore.Serialization
+{
+    public class JsonSerial : ISerial
+    {
+        private JsonSerializer Serializer
+        {
+            get
+            {
+                return new JsonSerializer()
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                };
+            }
+        }
+
+        public void Serialize(object value, Type type, StreamWriter streamWriter)
+        {
+            JsonTextWriter jsonTextWriter = new JsonTextWriter(streamWriter);
+            Serializer.Serialize(jsonTextWriter, value, type);
+            streamWriter.Flush();
+        }
+        public object Deserialize(Type type, StreamReader streamReader)
+        {
+            JsonTextReader jsonTextReader = new JsonTextReader(streamReader);
+            return Serializer.Deserialize(jsonTextReader, type);
+        }
+    }
+}
